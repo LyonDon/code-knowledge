95bf51f7db922f04e064ef011caa9677d2e1941d
LyonDon <wangyidl258@163.com>
2019-04-17 11:41:56 +0800

    添加了涉及模式文件夹，解释了不同设计模式的概念，以及部分重要设计模式的用法

---
+++ 设计模式/yauxingmodule.java
@@ -0,0 +1,45 @@
+/** Prototype Class **/
+ public class Cookie implements Cloneable {
+
+    public Object clone() throws CloneNotSupportedException
+    {
+        //In an actual implementation of this pattern you would now attach references to
+        //the expensive to produce parts from the copies that are held inside the prototype.
+        return (Cookie) super.clone();
+    }
+ }
+
+ /** Concrete Prototypes to clone **/
+ public class CoconutCookie extends Cookie { }
+
+ /** Client Class**/
+ public class CookieMachine
+ {
+
+   private Cookie cookie;//cookie必须是可复制的
+
+     public CookieMachine(Cookie cookie) { 
+         this.cookie = cookie; 
+     } 
+
+    public Cookie makeCookie()
+    {
+        try
+        {
+            return (Cookie) cookie.clone();
+        } catch (CloneNotSupportedException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    } 
+
+
+     public static void main(String args[]){ 
+         Cookie tempCookie =  null; 
+         Cookie prot = new CoconutCookie(); 
+         CookieMachine cm = new CookieMachine(prot); //设置原型
+         for(int i=0; i<100; i++) 
+             tempCookie = cm.makeCookie();//通过复制原型返回多个cookie 
+     } 
+ }
